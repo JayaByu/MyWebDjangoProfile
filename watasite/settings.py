2a5aae60b0a2dc190277cc5026138c2ff0d2c603
JayaByu <jayawardhanabayu@gmail.com>
2021-09-07 23:34:47 +0000

    changee

--- watasite/settings.py
+++ watasite/settings.py
@@ -24,11 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)$qgc7%4r9&=pht78wghn!u3u4at@em*qv^20z#o6x3cfilzf3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-# DEBUG = True
+# DEBUG = False
+DEBUG = True
 
+ALLOWED_HOSTS = []
 # ALLOWED_HOSTS = ['*']
-ALLOWED_HOST = ['127.0.0.1','bayuwebprofile.herokuapp.com/']
+# ALLOWED_HOSTS = ['127.0.0.1','bayuwebprofile.herokuapp.com/']
 
 # Application definition
 
@@ -122,9 +123,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'require'),
+    os.path.join(BASE_DIR,'static'),
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
